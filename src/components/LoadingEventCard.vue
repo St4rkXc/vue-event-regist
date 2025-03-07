9327f6c5e906b4d602d23813b3483fa9244cdf59
Stark <123723458+St4rkXc@users.noreply.github.com>
2025-03-07 22:14:34 +0800

    skeleton dan fixing loading components

---
+++ src/components/LoadingEventCard.vue
@@ -0,0 +1,23 @@
+<script setup>
+import SectionCard from './SectionCard.vue';
+</script>
+
+<template>
+    <SectionCard>
+        <template #header>
+            <div class="skeleton h-6 w-3/4"></div>
+        </template>
+        <template #default>
+            <div class="space-y-4">
+                <div class="skeleton h-3 w-[20%]"></div>
+            </div>
+        </template>
+        <template #footer>
+            <div class="space-y-4">
+                <div class="skeleton h-4 w-1/2"></div>
+                <div class="skeleton h-4 w-3/5"></div>
+                <div class="skeleton h-4 w-1/4"></div>
+            </div>
+        </template>
+    </SectionCard>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,20 @@
 <script setup>
 import EventCard from '@/components/EventCard.vue'
 import BookingItem from '@/components/BookingItem.vue';
+import LaodingEvent from '@/components/LoadingEventCard.vue';
 import { ref, onMounted } from 'vue';
 
 const events = ref([])
+const eventsLoading = ref(false)
 
 const fetchEvents = async () => {
-    const response = await fetch('http://localhost:3001/events')
-    events.value = await response.json()
-    console.log(events.value)
+    eventsLoading.value = true
+    try {
+        const response = await fetch('http://localhost:3001/events')
+        events.value = await response.json()
+    } finally {
+        eventsLoading.value = false
+    }
 }
 
 onMounted( () => {
@@ -18,18 +24,23 @@ onMounted( () => {
 </script>
 
 <template>
-    <div class="container mx-auto my-8 space-y-8">
+    <div class="container mx-auto my-8 space-y-8 px-2 ">
         <h1 class="text-4xl font-medium">Event Booking App</h1>
         <h2 class="text-2xl font-medium">All Events</h2>
-        <div class="grid grid-cols-2 gap-6">
-            <EventCard
-                v-for="event in events"
-                :key="event.id"
-                :title="event.title"
-                :when="event.date"
-                :desc="event.description"
-                @register="console.log('Registering...')"
-            />
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <template v-if="!eventsLoading">
+                <EventCard
+                    v-for="event in events"
+                    :key="event.id"
+                    :title="event.title"
+                    :when="event.date"
+                    :desc="event.description"
+                    @register="console.log('Registering...')"
+                />
+            </template>
+            <template v-else>
+                <LaodingEvent v-for="i in 4" :key="i"/>
+            </template>
         </div>
         <h2 class="text-2xl font-medium">Your Bookings</h2>
         <div class="space-y-4">
@@ -37,3 +48,4 @@ onMounted( () => {
         </div>
     </div>
 </template>
+
